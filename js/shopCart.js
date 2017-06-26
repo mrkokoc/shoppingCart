55d57bdf3063d0989e6d371ea2425151c9df309e
mrkokoc <ilia.guskov@gmail.com>
2017-06-26 17:33:46 +0300

    added jquery addCart/displayCart functions

--- js/shopCart.js
+++ js/shopCart.js
@@ -1,3 +1,26 @@
+$(".add-to-cart").click(function (event) {
+    event.preventDefault();
+    var name = $(this).attr("data-name");
+    var price = Number($(this).attr("data-price"));
+
+    addItemToCart(name, price, 1);
+    displayCart();
+});
+
+// fills <ul id="show-cart">
+function displayCart() {
+    var cartArray = listCart();
+    var output = '';
+    for (var i in cartArray) {
+        output += "<li>" + cartArray[i].name + " " + cartArray[i].count + "</li>";
+    }
+    $("#show-cart").html(output);
+}
+
+
+//  ***********************  //
+//  Shopping cart functions  //
+
 var cart = [];
 
 var Item = function (name, price, count) {
@@ -37,7 +60,7 @@ function removeItemFromCartAll(name) {
 function removeItemFromCart(name) {
     for (var i in cart) {
         if (cart[i].name === name) {
-            cart[i].count --;
+            cart[i].count--;
             if (cart[i].count === 0) {
                 cart.splice(i, 1);
             }
